1582c7232a81c892f18edd1905d5a07658ce519d
Eugene Kirpichov <ekirpichov@gmail.com>
2012-11-14 13:42:53 +0400

    Redirect / to /dashboard on RedHat to be consistent with Ubuntu

--- manifests/init.pp
+++ manifests/init.pp
@@ -78,6 +78,16 @@ class horizon(
      notify => Service["$::horizon::params::http_service"],
   }
 
+   if $::osfamily == 'RedHat'
+   {
+     file_line { 'horizon_redirect_rule':
+       path => $::horizon::params::httpd_config_file,
+       line => 'RedirectMatch permanent ^/$ /dashboard/',
+       require => Package["$::horizon::params::package_name"],
+       notify => Service["$::horizon::params::http_service"]
+     }
+   }
+ 
   file_line { 'horizon root':
     path => $::horizon::params::httpd_config_file,
     line => "WSGIScriptAlias ${::horizon::params::root_url} /usr/share/openstack-dashboard/openstack_dashboard/wsgi/django.wsgi",
